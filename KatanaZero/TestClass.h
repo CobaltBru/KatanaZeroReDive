ea603bfa00b3b1e92c069a355ab222a25384aad8
CobaltBru <cobaltbru123@gmail.com>
2025-04-12 11:06:30 +0900

    testClass(Image)

--- KatanaZero/TestClass.h
+++ KatanaZero/TestClass.h
@@ -2,7 +2,16 @@
 #include "config.h"
 class TestClass
 {
+private:
+	Gdiplus::Pen* pens[5];
 public:
-	VOID OnPaint(HDC hdc);
+	enum Pcolor { BLACK , WHITE , RED , GREEN , BLUE };
+
+	void Init();
+
+	void RenderRect(HDC hdc , FPOINT pos , float width , float height , Pcolor color);
+	void Middle_RenderRect(HDC hdc , FPOINT pos , float width , float height , Pcolor color);
+
+	void Release();
 };
 
