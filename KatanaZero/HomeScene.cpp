802b858c1493bb9449307966a5c6798d581f92f7
CobaltBru <97279958+CobaltBru@users.noreply.github.com>
2025-04-20 18:49:32 +0900

    home 제작중

--- KatanaZero/HomeScene.cpp
+++ KatanaZero/HomeScene.cpp
@@ -1,11 +1,13 @@
 #include "HomeScene.h"
 #include "CommonFunction.h"
 
+#include "Background.h"
 #include "Animation.h"
 #include "ObjectManager.h"
 #include "RenderManager.h"
 #include "ImageManager.h"
 #include "GPImage.h"
+#include "Image.h"
 
 HomeScene::HomeScene():
 	ObjectManager(nullptr), RenderManager(nullptr)
@@ -21,7 +23,7 @@ HRESULT HomeScene::Init()
 	RenderManager = RenderManager::GetInstance();
 	RenderManager->Init();
 
-	if (FAILED(InitImage()))
+	/*if (FAILED(InitImage()))
 	{
 		MessageBox(g_hWnd, TEXT("Stage1Scene InitImage Failed."), TEXT("실패"), MB_OK);
 		return E_FAIL;
@@ -31,7 +33,7 @@ HRESULT HomeScene::Init()
 	{
 		MessageBox(g_hWnd, TEXT("Stage1Scene InitObject Failed."), TEXT("실패"), MB_OK);
 		return E_FAIL;
-	}
+	}*/
 	return S_OK;
 }
 
@@ -61,19 +63,42 @@ void HomeScene::Render(HDC hdc)
 HRESULT HomeScene::InitImage()
 {
 
+	ImageManager::GetInstance()->AddImage("BlackBackground",L"Image/Background/black.bmp");
 	return S_OK;
 }
 
 HRESULT HomeScene::InitObject()
 {
-	GPImage* tmp = new GPImage();
-	tmp->AddImage(L"Image/UI/Home/spr_title_background.png");
+	Background* bg = new Background();
+	bg->Init("BlackBackground");
+	ObjectManager->AddGameObject(EObjectType::GameObject, bg);
+
+
+	Image* tmp = ImageManager::GetInstance()->AddImage("title_bg", L"Image/UI/Home/spr_title_background.bmp",1600,1800,1,1, true, RGB(255, 0, 255));
 	background = new Animation();
 	background->Init(tmp, 1);
 	background->setPos({ 0.f,-600.f }, false, false);
 	background->setAniTask({ {0,10.f} });
-	background->MoveOn({ 0.f,-300.f }, 1.0f, Move_SoftEnd | Move_Stop);
+	background->MoveOn({ 0.f,-300.f }, 3.0f, Move_SoftEnd | Move_Stop);
 	background->On();
 	ObjectManager->AddGameObject(EObjectType::GameObject, background);
+
+	tmp = ImageManager::GetInstance()->AddImage("title_grass1", L"Image/UI/Home/spr_grass1.bmp", 1600, 300, 1, 1, true, RGB(255, 0, 255));
+	grass1 = new Animation();
+	grass1->Init(tmp, 1);
+	grass1->setPos({ 0.f,WINSIZE_Y}, false, false);
+	grass1->setAniTask({ {0,10.f} });
+	grass1->MoveOn({ 0.f,-300.f }, 3.0f, Move_SoftEnd | Move_Stop);
+	grass1->On();
+	ObjectManager->AddGameObject(EObjectType::GameObject, grass1);
+
+	/*Image* tmp1 = ImageManager::GetInstance()->AddImage("title_grass1", L"Image/UI/Home/spr_grass1.bmp", 1600, 300, 1, 1, true, RGB(255, 0, 255));
+	fence = new Animation();
+	fence->Init(tmp1, 1);
+	fence->setPos({ 0.f,WINSIZE_Y }, false, false);
+	fence->setAniTask({ {0,10.f} });
+	fence->MoveOn({ 0.f,-300.f }, 3.0f, Move_SoftEnd | Move_Stop);
+	fence->On();
+	ObjectManager->AddGameObject(EObjectType::GameObject, fence);*/
 	return S_OK;
 }
