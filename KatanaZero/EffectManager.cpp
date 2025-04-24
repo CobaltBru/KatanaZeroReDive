93c04ea6613bded84d3b74a467fb71eb98e01197
AHY <ahy3809@gmail.com>
2025-04-24 17:41:41 +0900

    feat.프레임타임주석

--- KatanaZero/EffectManager.cpp
+++ KatanaZero/EffectManager.cpp
@@ -14,6 +14,7 @@ void EffectManager::Init()
 
 void EffectManager::InitEffects()
 {
+    // 디폴트 프레임 업데이트 시간 0.1초
     Addfx("gangstergun", {}, {}, 0.f);
     Addfx("normalslash", {}, {}, 0.f);
     Addfx("rainbowslash", {}, {}, 0.f);
